cb2c3af518c07d74a9044213a81e37ae199e54f9
kppalmo <kpalmo@ithaca.edu>
2022-04-01 11:06:42 -0400

    customer class implemented

---
+++ src/main/java/edu/ithaca/dturnbull/bank/Customer.java
@@ -0,0 +1,45 @@
+package edu.ithaca.dturnbull.bank;
+
+public class Customer {
+
+    public String name;
+    public Double id;
+    public String status;
+    public Double balance;
+    public Customer (String name, Double id, Double balance, String status){
+        this.name = name;
+        this.id = id;
+        this.balance = balance;
+        //green = can bid   red = on blacklist, cannot bid
+        this.status = "green";
+    }
+
+
+    //Getter for customer balance
+    public double getCustomerBalance(){
+        return balance;
+    }
+
+    //Place bid on Item / checks for status & bidAmount is available 
+    public void placeBid(Item obj, Double bidAmount){
+        if( status == "green"){
+            if(bidAmount <= balance && Item.startingBid <= bidAmount){
+                Item.currBid = bidAmount;
+                balance -= bidAmount;
+            }
+        }
+    }
+
+    //Withdraw bid from item
+    public void withdrawBid(Item obj, Double bidAmount){
+        if (status == "green"){
+            Item.currBid -= bidAmount;
+            balance += bidAmount;
+
+        }
+
+    }
+
+
+    
+}
