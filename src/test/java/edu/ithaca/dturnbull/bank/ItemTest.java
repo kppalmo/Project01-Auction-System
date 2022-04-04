c1899c64e0de3d94ba533bfad1f7480a143720ec
vkumar825 <vkumar825@users.noreply.github.com>
2022-04-04 11:35:12 -0400

    wrote tests for bidHistory

---
+++ src/test/java/edu/ithaca/dturnbull/bank/ItemTest.java
@@ -0,0 +1,32 @@
+package edu.ithaca.dturnbull.bank;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ItemTest {
+
+    @Test
+    void bidHistoryTest(){
+        Customer cust = new Customer("Vivek", 24.0, 150.00, "green");
+        Item testOBJ = new Item(15.00, "str", 15.00);
+
+        cust.placeBid(testOBJ, 20.00);
+        assertEquals(20.00, testOBJ.getCurrBid());
+        assertEquals(130.00, cust.getCustomerBalance());
+
+        Customer cust2 = new Customer("Keegan", 24.0, 150.00, "green");
+
+        cust2.placeBid(testOBJ, 60.00);
+        assertEquals(60.00, testOBJ.getCurrBid());
+        assertEquals(90.00, cust2.getCustomerBalance());
+
+        cust2.withdrawBid(testOBJ, 60.00);
+        assertEquals(150.00, cust2.getCustomerBalance());
+        assertEquals(20.00, testOBJ.getCurrBid());
+
+
+
+
+    } 
+    
+}
