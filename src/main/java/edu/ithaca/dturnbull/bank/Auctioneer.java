ea60ca9fee5f6aacbd3389332c35d9e42658c15b
kppalmo <kpalmo@ithaca.edu>
2022-04-07 16:36:09 -0400

    merged branches / moved files to correct folders

--- src/main/java/edu/ithaca/dturnbull/bank/Auctioneer.java
+++ src/main/java/edu/ithaca/dturnbull/bank/Auctioneer.java
@@ -1,4 +1,4 @@
-package edu.ithaca.dturnbull;
+package edu.ithaca.dturnbull.bank;
 
 import java.util.Random;
 
--- src/test/java/edu/ithaca/dturnbull/bank/AuctioneerTests.java
+++ src/test/java/edu/ithaca/dturnbull/bank/AuctioneerTests.java
@@ -1,7 +1,8 @@
 
-package edu.ithaca.dturnbull;
+package edu.ithaca.dturnbull.bank;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AuctioneerTests {
