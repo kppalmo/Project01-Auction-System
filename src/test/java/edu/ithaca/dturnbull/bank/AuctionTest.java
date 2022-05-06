2ef882c9a9e4239d83cab0a4a72f706f1b69bfba
mateoh6 <89436949+mateoh6@users.noreply.github.com>
2022-05-06 02:28:38 -0400

    Updated tests to work with changes

--- src/test/java/edu/ithaca/dturnbull/bank/AuctionTest.java
+++ src/test/java/edu/ithaca/dturnbull/bank/AuctionTest.java
@@ -8,24 +8,28 @@ public class AuctionTest {
     // Tests to insure all parts of Customer are working
     @Test
     void validCustomerTest() {
-        Customer cust = new Customer("Keegan", 34.0, 150.00, "green",false);
+        Customer cust = new Customer("Vivek", 0, 150.0, "green");
         assertEquals(150, cust.getCustomerBalance());
         assertEquals("green", cust.status);
-        assertEquals(34.0, cust.id);
+        assertEquals(0, cust.id);
     }
 
     // Tests to check place bid on Item
     @Test
     void placeBidTest() throws InsufficientFundsException {
-        Customer cust = new Customer("Vivek", 24.0, 150.00, "green",true);
-        Customer cust2 = new Customer("Keegan", 2.0, 100.00, "green",true);
-        Item testOBJ = new Item("test",20.00, "str", 15.00,0);
-        cust.placeBid(testOBJ, 25.00);
-        assertEquals(25.00, testOBJ.getCurrBid());
-        cust2.placeBid(testOBJ, 35.00);
-        assertEquals(35.00, testOBJ.getCurrBid());
-        cust2.withdrawBid(testOBJ, 35.00);
-        assertEquals(25.00, testOBJ.getCurrBid());
+        Customer cust = new Customer("Vivek", 0, 150.0, "green");
+        Customer cust2 = new Customer("Keegan", 1, 100.0, "green");
+        Auctioneer auctioneer = new Auctioneer("jim", 25);
+        auctioneer.startAuction(25,0,"Auction 0");
+        auctioneer.placeItem(0, "test", 15.00, "str");
+        cust.placeBid(auctioneer.getCurrentItem(0),  25.00, 0, 1);
+        //Makes sure that place bid worked correctly
+        assertEquals(25.00, auctioneer.getCurrentItem(0).getCurrBid());
+        cust2.placeBid(auctioneer.getCurrentItem(0),  35.00, 0, 2);
+        assertEquals(35.00, auctioneer.getCurrentItem(0).getCurrBid());
+        cust2.withdrawBid(auctioneer.getCurrentItem(0));
+        //Makes sure that withdraw bid works correctly
+        assertEquals(25.00, auctioneer.getCurrentItem(0).getCurrBid());
 
     }
 
--- src/test/java/edu/ithaca/dturnbull/bank/ItemTest.java
+++ src/test/java/edu/ithaca/dturnbull/bank/ItemTest.java
@@ -7,31 +7,31 @@ public class ItemTest {
 
     @Test
     void bidHistoryTest() throws InsufficientFundsException{
-        Customer cust = new Customer("Vivek", 24.0, 150.00, "green",true);
-        Item testOBJ = new Item("test",15.00, "str", 15.00,0);
+        Customer cust = new Customer("Vivek", 0, 150.0, "green");
+        Auctioneer auctioneer = new Auctioneer("jim", 25);
+        auctioneer.startAuction(25,5,"Auction 5");
+
+        auctioneer.placeItem(5, "test", 15.00, "str");
 
         // check that the first bid in the stack is the starting bid
-        assertEquals(15.00, testOBJ.getCurrBid()); 
+        assertEquals(15.00, auctioneer.getCurrentItem(5).getCurrBid()); 
 
         // cannot withdraw the starting bid
-        assertThrows(IllegalArgumentException.class, ()-> cust.withdrawBid(testOBJ, 15.00));
-
-        cust.placeBid(testOBJ, 20.00);
-        assertEquals(20.00, testOBJ.getCurrBid());
+        assertThrows(IllegalArgumentException.class, ()-> cust.withdrawBid(auctioneer.getCurrentItem(5)));
+        cust.placeBid(auctioneer.getCurrentItem(5),  20.00, 0, 1);
+        assertEquals(20.00, auctioneer.getCurrentItem(5).getCurrBid());
         assertEquals(130.00, cust.getCustomerBalance());
 
-        Customer cust2 = new Customer("Keegan", 24.0, 150.00, "green",true);
-
-        cust2.placeBid(testOBJ, 60.00);
-        assertEquals(60.00, testOBJ.getCurrBid());
+        Customer cust2 = new Customer("Keegan", 1, 150.0, "green");
+        cust2.placeBid(auctioneer.getCurrentItem(5),  60.00, 5, 2);
+        assertEquals(60.00, auctioneer.getCurrentItem(5).getCurrBid());
         assertEquals(90.00, cust2.getCustomerBalance());
 
         // check that exception is thrown if a customer tries to place a bid lower than the current bid
-        assertThrows(InsufficientFundsException.class, ()-> cust.placeBid(testOBJ, 10.00));
-
-        cust2.withdrawBid(testOBJ, 60.00);
+        assertThrows(InsufficientFundsException.class, ()-> cust.placeBid(auctioneer.getCurrentItem(5),  10.00, 5, 3));
+        cust2.withdrawBid(auctioneer.getCurrentItem(5));
         assertEquals(150.00, cust2.getCustomerBalance());
-        assertEquals(20.00, testOBJ.getCurrBid());
+        assertEquals(20.00, auctioneer.getCurrentItem(5).getCurrBid());
 
 
 
