d88aea333bb20f6f190a29b3b2eb726d6540dd47
vkumar825 <vkumar825@users.noreply.github.com>
2022-05-05 21:30:11 -0400

    revised CollectionOfAuctions

--- src/main/java/edu/ithaca/dturnbull/bank/CollectionOfAuctions.java
+++ src/main/java/edu/ithaca/dturnbull/bank/CollectionOfAuctions.java
@@ -2,13 +2,17 @@ package edu.ithaca.dturnbull.bank;
 
 import java.util.HashMap;
 
-public class CollectionOfAuctions extends Auction {
-    public CollectionOfAuctions(boolean auctionStatus, int auctionId) {
-        super(auctionStatus, auctionId);
-        //TODO Auto-generated constructor stub
-    }
+public class CollectionOfAuctions {
+    // public CollectionOfAuctions(boolean auctionStatus, int auctionId) {
+    //     super(auctionStatus, auctionId);
+    //     //TODO Auto-generated constructor stub
+    // }
 
     //use the auction id and auction to store different auctions
     static HashMap<Integer, Auction> collectionOfAuctions=new HashMap<Integer,Auction>();
+
+    public static HashMap<Integer, Auction> getCollectionOfAuctions(){
+        return collectionOfAuctions;
+    }
 
 }
